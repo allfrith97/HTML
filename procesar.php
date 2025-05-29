3e9bf3c6b7115d58822f6a5cb15d833053c9bb3a
allfrith <ing.josejimenezper@gmail.com>
2025-05-29 00:39:41 -0600

    Agregando mis practicas en HTML y CSS

---
+++ procesar.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formularios</title>
+</head>
+<body>
+    <section>
+    <?php
+    print('El valor es:' .$_GET['val']);
+?>
+    </section>
+</body>
+</html>
